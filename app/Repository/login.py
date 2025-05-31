9ff5f66a62081d028dbbfd8af828f6345f43a68c
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-05-31 13:37:08 +0100

    Imported a new library

--- app/Repository/login.py
+++ app/Repository/login.py
@@ -2,10 +2,11 @@ from ..databases import Session
 from fastapi import status,HTTPException
 from ..models import User_detail
 from ..hash import Hash
+from sqlalchemy import func
 
 def login_user(request,db:Session):
     username=request.Username.strip().lower()
-    user=db.query(User_detail).filter(User_detail.Username==username).first()
+    user=db.query(User_detail).filter(func.lower(User_detail.Username)==username).first()
     if not user :
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User Not Found")
     elif not Hash.verify_hash(request.Password,user.Password):
