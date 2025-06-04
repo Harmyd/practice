cfbb4fa67961a2ffbb4d9a3407fc68c9bad57254
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-06-04 17:09:37 +0100

    k

--- app/Repository/login.py
+++ app/Repository/login.py
@@ -12,21 +12,18 @@ def login_user(request,db:Session):
     user=db.query(User_detail).filter(func.lower(User_detail.Username)==username).first()
     if not user:
         return JSONResponse(
-            content={"message":"user not found"},
-            status_code=status.HTTP_404_NOT_FOUND
-            
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message":"user not found"}
         ) 
         #raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User Not Found")
     elif not Hash.verify_hash(request.Password,user.Password):
         return JSONResponse(
-            content={"message":"wrong password"},
-            status_code=status.HTTP_404_NOT_FOUND
-            
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message":"wrong password"}
         )
         #raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Wrong Password")
     else:
         return JSONResponse(
-            
             status_code=status.HTTP_200_OK,
             content= {"message":"Login Successful",
                     "user_id":user.id,
