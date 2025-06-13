ebca5aa19cd10d6a06f3bddc2d7eff38b1bcb554
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-06-13 16:56:24 +0100

    k

--- app/Repository/Tasks.py
+++ app/Repository/Tasks.py
@@ -35,7 +35,7 @@ def get_task_for_user(id,db:Session):
     tasks=db.query(models.Task).filter(models.Task.User_id==id).order_by(models.Task.id.asc()).all()
     if not tasks:
         return JSONResponse(
-            status_code=status.HTTP_404_NOT_FOUND,
+           
             content={"message":"No Task found for this user"}
         )
     return JSONResponse(
