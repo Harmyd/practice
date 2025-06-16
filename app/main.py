5e2bae5d1f29396ec30a90b5c9750b2e3d8a786e
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-06-16 23:00:58 +0100

    p

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ origin=[
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origin],
+    allow_origins=origin,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
