e195423bd36ffe947c57497552d507f577306f47
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-06-16 17:47:19 +0100

    kl

--- app/Token.py
+++ app/Token.py
@@ -8,9 +8,9 @@ from jose import JWTError,jwt
 def create_access_token(data:dict,expiry_delta:Optional[timedelta]=None):
     to_encode=data.copy()
     if expiry_delta:
-        expiry=datetime.utcnow + expiry_delta
+        expiry=datetime.utcnow() + expiry_delta
     else:
-        expiry = datetime.utcnow + timedelta(minutes=15)
+        expiry = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp":expiry})
     encoded_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
     return encoded_jwt
