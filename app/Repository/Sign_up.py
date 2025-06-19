17f1a483d8da76c1c84e8a7af94dcb5f1f6d5354
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-06-19 23:42:34 +0100

    Arranged my signup response

--- app/Repository/Sign_up.py
+++ app/Repository/Sign_up.py
@@ -1,9 +1,11 @@
 from fastapi import HTTPException,status
+from fastapi.responses import JSONResponse
 from ..models import User_detail
 from ..databases import Session
 from ..schemas import User
 from ..hash import Hash
 from fastapi.responses import PlainTextResponse
+from ..Token import create_access_token
 
 def signUp(request,db:Session):
     Username=request.Username.strip()
@@ -22,7 +24,14 @@ def signUp(request,db:Session):
             db.add(new_user)
             db.commit()
             db.refresh(new_user)
-            return new_user
+            return JSONResponse(
+                status_code=status.HTTP_201_CREATED,
+                content={
+                            "message":"Signed up successfully",
+                            "Token":create_access_token(data={"user_id":new_user.id,"username":new_user.Username}),
+                            "Token_type":"Bearer"
+                         }
+            )
         except Exception as e:
             db.rollback()
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail=str(e))
